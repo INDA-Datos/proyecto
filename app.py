cc2f8fe3fe1864af23e01c90918dd012c3a1debd
francoleon <fleonr3@upao.edu.pe>
2024-11-27 04:35:58 -0500

    hola

--- app.py
+++ app.py
@@ -296,6 +296,5 @@ def process_cleaning():
         print("Error durante la limpieza:", e)
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    plt.switch_backend('agg')
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
